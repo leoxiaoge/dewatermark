bf514de253182fa93f95e7881fc0516b02848f95
leoxiaoge <1269286430@qq.com>
2021-10-26 19:25:49 +0800

    commit

---
+++ miniprogram/app.js
@@ -0,0 +1,44 @@
+//app.js
+var util = require('./utils/util.js');
+var api = require('./config/api.js');
+var user = require('./utils/user.js');
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        // env 参数说明：
+        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        //   如不填则使用默认环境（第一个创建的环境）
+        // env: 'my-env-id',
+        traceUser: true,
+      })
+    }
+    const updateManager = wx.getUpdateManager();
+    wx.getUpdateManager().onUpdateReady(function() {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    this.globalData = {}
+  },
+  onShow: function(options) {
+    user.checkLogin().then(res => {
+      this.globalData.hasLogin = true;
+    }).catch(() => {
+      this.globalData.hasLogin = false;
+    });
+  },
+  globalData: {
+    hasLogin: false
+  }
+})
---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,163 @@
+//index.js
+const app = getApp()
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+
+Page({
+  data: {
+    // 轮播图
+    banner: [{
+      url: '../../static/images/banner1.png'
+    }],
+    // 视频名称
+    nickname: '',
+    // 文字
+    desc: '',
+    // 链接
+    url: 'https://v.douyin.com/dbkYKmK/',
+    // 视频
+    video: ''
+  },
+
+  onLoad: function () {
+    this.searchTap()
+  },
+
+  onShow: function () {
+    wx.getClipboardData({
+      success: (res) => {
+        // 匹配地址
+        let result = util.handleUrl(res.data)
+        if (!result) {
+          return
+        }
+        // 如果地址相同则不在显示
+        if (result == this.data.url) {
+          return
+        }
+        wx.showModal({
+          title: '检测到视频链接，是否粘贴？',
+          content: result,
+          showCancel: true, //是否显示取消按钮
+          cancelText: "取消", //默认是“取消”
+          cancelColor: '#fe6b35', //取消文字的颜色
+          confirmText: "粘贴", //默认是“确定”
+          confirmColor: '#fe6b35', //确定文字的颜色
+          success: (res) => {
+            if (res.cancel) {} else {
+              this.setData({
+                url: result,
+              })
+            }
+          },
+        })
+      }
+    })
+  },
+
+  // 搜索框事件
+  bindKeyInput: function (e) {
+    this.setData({
+      url: e.detail.value
+    })
+  },
+
+  // 清除事件
+  closeTap() {
+    this.setData({
+      url: ''
+    })
+  },
+
+  // 解析事件
+  searchTap() {
+    let url = this.data.url
+    if (!url) {
+      wx.showModal({
+        title: '温馨提示',
+        content: '网址不能为空',
+        showCancel: false,
+        confirmColor: '#fe6b35',
+        success(res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+      return
+    }
+    wx.showLoading({
+      title: '加载中...',
+    })
+    let data = {
+      url: url
+    }
+    util.request(api.IndexUrl, data).then(res => {
+      wx.hideLoading()
+      if (res.status === "success") {
+        let video = res.mp4
+        let nickname = res.nickname
+        let desc = res.desc
+        this.setData({
+          video: video,
+          nickname: nickname,
+          desc: desc
+        })
+      }
+    });
+  },
+
+  // 下载视频
+  downloadFile(e) {
+    let url = this.data.video;
+    // let fullName = this.data.nickname
+    wx.downloadFile({
+      url: url,
+      // filePath: wx.env.USER_DATA_PATH + '/' + fullName,
+      success(res) {
+        if (res.statusCode === 200) {
+          wx.hideLoading()
+          // let tempFilePath = res.filePath  // 如果设置了filePath参数，则不会有tempFilePath
+          let tempFilePath = res.tempFilePath
+          console.log('tempFilePath', tempFilePath, res)
+          wx.saveFile({
+            tempFilePath,
+            success(res) {
+              // 可以进行到这里
+              console.log(res);
+              const savedFilePath = res.savedFilePath
+              console.log(savedFilePath)
+              wx.showToast({
+                title: '下载成功',
+                icon: 'none',
+                mask: true
+              })
+            },
+            fail(err) {
+              console.log(err);
+              wx.showToast({
+                title: '下载失败，请重新尝试',
+                icon: 'none',
+                mask: true
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  // 复制链接
+  setClipboardData() {
+    wx.setClipboardData({
+      data: this.data.video,
+      success() {
+        wx.showToast({
+          title: '复制成功',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+    })
+  }
+})
